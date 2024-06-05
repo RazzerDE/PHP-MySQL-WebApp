5da8101104a6182ad53da4a4a20232eba199a8c4
Doepner, Yanic <yanic.doepner@sulzer.de>
2024-06-05 07:31:42 +0200

    rollback $current_table

--- assets/php/table.php
+++ assets/php/table.php
@@ -3,7 +3,7 @@ require_once 'assets/php/main.php';
 
 $tableData = [];
 
-$current_table = $_GET['dropdownSelect'] ?? "Wähle eine Tabelle in dem rechten Dropdown-Menü aus.";
+$current_table = "Wähle eine Tabelle in dem rechten Dropdown-Menü aus.";
 
 // get Table data
 function getAllTableData($column = null, $statement = null): array {
