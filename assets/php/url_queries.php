a06c818288d9070d5db54ad13c232d0606971326
yanic <yanic.dev@gmail.com>
2024-06-07 21:19:20 +0200

    refactor url_queries.php

--- assets/php/url_queries.php
+++ assets/php/url_queries.php
@@ -6,137 +6,123 @@ function getTableDataByURL() {
     global $conn;
     global $tableData;
 
-    // change sql statement if another one is needed
-    if (isset($_GET['sql_statement'])) {
-        getAllTableData(null, $_GET['sql_statement']);
-    } else if (isset($_GET['dropdownSelect'])) {
-        $table = $_GET['dropdownSelect'];
-        getAllTableData(null, 'SELECT * FROM '.$table);
-    } else {
-        getAllTableData();
-    }
+    $selectedTable = 'buecher';
+    $sql = 'SELECT * FROM '.$selectedTable;
 
     if (isset($_GET['sql_statement'])) {
-      $sql = $_GET['sql_statement'];
-
-      preg_match('/FROM\s+(\w+)/i', $sql, $matches);
-      $selectedTable = $matches[1];
-
+        $sql = $_GET['sql_statement'];
+        preg_match('/FROM\s+(\w+)/i', $sql, $matches);
+        $selectedTable = $matches[1];
     } else if (isset($_GET['dropdownSelect'])) {
         $selectedTable = $_GET['dropdownSelect'];
-    } else {
-        $selectedTable = 'buecher';
+        $sql = 'SELECT * FROM '.$selectedTable;
     }
 
-    $fields = [];
-    switch ($selectedTable) {
-        case 'autoren':
-            $fields = ['autoren_id', 'vorname', 'nachname', 'geburtsdatum'];
-            break;
-        case 'autoren_has_buecher':
-            $fields = ['autoren_autoren_id', 'buecher_buecher_id'];
-            break;
-        case 'buecher':
-            $fields = ['buecher_id', 'titel', 'verkaufspreis', 'einkaufspreis', 'erscheinungsjahr', 'verlage_verlage_id'];
-            break;
-        case 'buecher_has_lieferanten':
-            $fields = ['buecher_buecher_id', 'lieferanten_lieferanten_id'];
-            break;
-        case 'buecher_has_sparten':
-            $fields = ['buecher_buecher_id', 'sparten_sparten_id'];
-            break;
-        case 'lieferanten':
-            $fields = ['lieferanten_id', 'name', 'orte_orte_id'];
-            break;
-        case 'orte':
-            $fields = ['orte_id', 'postleitzahl', 'name'];
-            break;
-        case 'sparten':
-            $fields = ['sparten_id', 'bezeichnung'];
-            break;
-        case 'verlage':
-            $fields = ['verlage_id', 'name', 'orte_orte_id'];
-            break;
-    }
+    getAllTableData(null, $sql);
+
+    $fields = getFieldsForTable($selectedTable);
 
     if (isset($_POST['newRow'])) {
-        $newRowValues = $_POST['newRow'];
-
-        $columns = implode(", ", $fields);
-        $placeholders = rtrim(str_repeat('?, ', count($newRowValues)), ', ');
-
-        $newRowValues = str_replace(",",".", $newRowValues);
-
-        // Prepare sql statement
-        $sql = "INSERT INTO $selectedTable ($columns) VALUES ($placeholders)";
-
-        $stmt = $conn->prepare($sql);
-
-        $types = str_repeat('s', count($newRowValues)); // 's' mean string
-        $stmt->bind_param($types, ...$newRowValues);
-
-        try {
-            // Versuchen Sie, das Statement auszuführen
-            $stmt->execute();
-            header("Refresh:0");
-            exit();
-        } catch (mysqli_sql_exception $e) {
-            // Fangen Sie den Fehler ab und behandeln Sie ihn
-            if (!checkDatesInArray($newRowValues)) {
-                echo "<p class='text-center bg-red-700 border rounded border-gray-700'>". "Datum wurde nicht Regelkonform eingegeben YYYY-MM-DD". "</p>";
-            } else {
-                echo "<p class='text-center bg-red-700 border rounded border-gray-700'>" . "Fehler beim Ausführen des SQL-Statements: " . $e->getMessage() . "</p>";
-            }
-        }
-
+        insertNewRow($selectedTable, $fields);
     }
 
     if (isset($_POST['editRow'])) {
-        $newRowValues = explode(',', $_POST['inputValues']);
-        $id = $_POST['editRow'];
-
-        $fields = implode(", ", $fields);
-
-        $newString = strstr($fields, ',');
-        $fields = explode(',', ltrim($newString, ', '));
-
-        $setStatement = "";
-        for ($i = 0; $i < count($fields); $i++) {
-            // Trim the values and add quotes around them if they are strings
-            $value = trim($newRowValues[$i]);
-            if (!is_numeric($value)) {
-                $value = "'" . $conn->real_escape_string($value) . "'";
-            }
-            $setStatement .= trim($fields[$i]) . " = " . $value;
-            if ($i != count($fields) - 1) {
-                $setStatement .= ", ";
-            }
-        }
-
-        // Prepare sql statement
-        $sql = "UPDATE $selectedTable SET ". $setStatement ." WHERE ". $id;
-
-        $stmt = $conn->prepare($sql);
-
-        try {
-            // Versuchen Sie, das Statement auszuführen
-            $stmt->execute();
-            header("Refresh:0");
-            exit();
-        } catch (mysqli_sql_exception $e) {
-            // Fangen Sie den Fehler ab und behandeln Sie ihn
-            if (!checkDatesInArray($newRowValues)) {
-                echo "<p class='text-center bg-red-700 border rounded border-gray-700'>". "Datum wurde nicht Regelkonform eingegeben YYYY-MM-DD". "</p>";
-            } else {
-                echo "<p class='text-center bg-red-700 border rounded border-gray-700'>" . "Fehler beim Ausführen des SQL-Statements: " . $e->getMessage() . "</p>";
-            }
-        }
+        updateRow($selectedTable, $fields);
     }
-
 
     // reset database if empty
     if (empty($tableData)) {
         resetDB();
+    }
+}
+
+function getFieldsForTable($selectedTable) {
+    $fields = [];
+    $tables = [
+        'autoren' => ['autoren_id', 'vorname', 'nachname', 'geburtsdatum'],
+        'autoren_has_buecher' => ['autoren_autoren_id', 'buecher_buecher_id'],
+        'buecher' => ['buecher_id', 'titel', 'verkaufspreis', 'einkaufspreis', 'erscheinungsjahr', 'verlage_verlage_id'],
+        'buecher_has_lieferanten' => ['buecher_buecher_id', 'lieferanten_lieferanten_id'],
+        'buecher_has_sparten' => ['buecher_buecher_id', 'sparten_sparten_id'],
+        'lieferanten' => ['lieferanten_id', 'name', 'orte_orte_id'],
+        'orte' => ['orte_id', 'postleitzahl', 'name'],
+        'sparten' => ['sparten_id', 'bezeichnung'],
+        'verlage' => ['verlage_id', 'name', 'orte_orte_id']
+    ];
+    if (array_key_exists($selectedTable, $tables)) {
+        $fields = $tables[$selectedTable];
+    }
+    return $fields;
+}
+
+function insertNewRow($selectedTable, $fields) {
+    global $conn;
+
+    $newRowValues = $_POST['newRow'];
+    $columns = implode(", ", $fields);
+    $placeholders = rtrim(str_repeat('?, ', count($newRowValues)), ', ');
+
+    $newRowValues = str_replace(",",".", $newRowValues);
+
+    // Prepare sql statement
+    $sql = "INSERT INTO $selectedTable ($columns) VALUES ($placeholders)";
+
+    $stmt = $conn->prepare($sql);
+
+    $types = str_repeat('s', count($newRowValues)); // 's' mean string
+    $stmt->bind_param($types, ...$newRowValues);
+
+    try {
+        // Versuchen Sie, das Statement auszuführen
+        $stmt->execute();
+        header("Refresh:0");
+        exit();
+    } catch (mysqli_sql_exception $e) {
+        // Fangen Sie den Fehler ab und behandeln Sie ihn
+        handleSqlException($e, $newRowValues);
+    }
+}
+
+function updateRow($selectedTable, $fields) {
+    global $conn;
+
+    $newRowValues = explode(',', $_POST['inputValues']);
+    $id = $_POST['editRow'];
+
+    $setStatement = "";
+    for ($i = 0; $i < count($fields); $i++) {
+        // Trim the values and add quotes around them if they are strings
+        $value = trim($newRowValues[$i]);
+        if (!is_numeric($value)) {
+            $value = "'" . $conn->real_escape_string($value) . "'";
+        }
+        $setStatement .= trim($fields[$i]) . " = " . $value;
+        if ($i != count($fields) - 1) {
+            $setStatement .= ", ";
+        }
+    }
+
+    // Prepare sql statement
+    $sql = "UPDATE $selectedTable SET ". $setStatement ." WHERE ". $id;
+
+    $stmt = $conn->prepare($sql);
+
+    try {
+        // Versuchen Sie, das Statement auszuführen
+        $stmt->execute();
+        header("Refresh:0");
+        exit();
+    } catch (mysqli_sql_exception $e) {
+        // Fangen Sie den Fehler ab und behandeln Sie ihn
+        handleSqlException($e, $newRowValues);
+    }
+}
+
+function handleSqlException($e, $newRowValues) {
+    if (!checkDatesInArray($newRowValues)) {
+        echo "<p class='text-center bg-red-700 border rounded border-gray-700'>". "Datum wurde nicht Regelkonform eingegeben YYYY-MM-DD". "</p>";
+    } else {
+        echo "<p class='text-center bg-red-700 border rounded border-gray-700'>" . "Fehler beim Ausführen des SQL-Statements: " . $e->getMessage() . "</p>";
     }
 }
 
@@ -150,11 +136,7 @@ function checkDatesInArray($array) {
             // Überprüfen, ob das Datum gültig ist
             if (!checkdate((int)$month, (int)$day, (int)$year)) {
                 return false;
-            } else {
-                return true;
             }
-        } else {
-            return true;
         }
     }
     return true;
